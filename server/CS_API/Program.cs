da855ff914d6ffd2fd9fc0631feb37ca4b52ffaa
krino45 <krina645@gmail.com>
2024-09-20 19:53:12 +0700

    Make sure backend C# returns JSON format files

--- server/CS_API/Program.cs
+++ server/CS_API/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using System.Threading.Tasks;
+using MyApi.Controllers;
 
 namespace MyApi
 {
@@ -14,8 +15,13 @@ namespace MyApi
             var app = builder.Build();
             app.UseCors(policy => policy
             .AllowAnyOrigin());
-            
-            app.MapGet("/api/preferences", async () => await TagService.GetTagCollectionJSON()); 
+
+            app.MapGet("/api/preferences", async (TagController tagService) =>
+            {
+                var jsonResult = await tagService.GetTagCollectionJSON();
+                return Results.Ok(jsonResult);
+            });
+
             app.Run("http://localhost:5258");
         }
     }
