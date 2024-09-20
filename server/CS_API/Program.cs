76c564e493c08d12985253311fe2d259e2456cbb
krino45 <krina645@gmail.com>
2024-09-20 20:09:56 +0700

    Revert backend change

--- server/CS_API/Program.cs
+++ server/CS_API/Program.cs
@@ -1,28 +1,31 @@
-using System.Text.Json;
-using System.Threading.Tasks;
-using MyApi.Controllers;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+using MongoDB.Driver;
 
-namespace MyApi
-{
-    public class Program
+    namespace MyApi
     {
-        public static void Main(string[] args)
+        public class Program
         {
-            var builder = WebApplication.CreateBuilder(args);
+            public static void Main(string[] args)
+            {
+                var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddCors();
+                builder.Services.AddCors();
+                builder.Services.AddSingleton<IMongoClient>(s => new MongoClient(Environment.GetEnvironmentVariable("MONGODB_URI")));
+                builder.Services.AddScoped<TagService>();
 
             var app = builder.Build();
-            app.UseCors(policy => policy
-            .AllowAnyOrigin());
+                app.UseCors(policy => policy
+                .AllowAnyOrigin());
 
-            app.MapGet("/api/preferences", async (TagController tagService) =>
-            {
-                var jsonResult = await tagService.GetTagCollectionJSON();
-                return Results.Ok(jsonResult);
-            });
+                app.MapGet("/api/preferences", async (TagService tagService) =>
+                {
+                    var jsonResult = await tagService.GetTagCollectionJSON();
+                    return Results.Ok(jsonResult);
+                });
+
 
             app.Run("http://localhost:5258");
+            }
         }
     }
-}
