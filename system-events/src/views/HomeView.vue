ed8e4863509d75b586f149489a0895bb45a25343
dnomokonov <zmnomokon715@gmail.com>
2024-09-20 16:07:30 +0700

    Add base views

---
+++ system-events/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Home pages</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ system-events/src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Страница с авторизацией</h1>
+</template>
+
+<style scoped>
+
+</style>
