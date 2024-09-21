fbf01f0dc96c95a4445b116816dfc16e5be950cd
Yuki <ghenyadog@bk.ru>
2024-09-21 18:59:24 +0700

    Fix repos

--- server/CS_API/src/Models/User.cs
+++ server/CS_API/src/Models/User.cs
@@ -1,4 +1,4 @@
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
@@ -37,7 +37,7 @@ namespace MyApi.Models
         public string? Role { get; set; }
 
         [BsonElement("preferences")]
-        public List<PreferenceTag>? Preferences { get; set; }
+        public List<string>? Preferences { get; set; }
 
         [BsonElement("profession")]
         public string? Profession { get; set; }
@@ -46,12 +46,12 @@ namespace MyApi.Models
         public int? RoundtableId { get; set; }
 
         [BsonElement("events")]
-        public List<BaseEvent>? Events { get; set; }
+        public List<ObjectId>? Events { get; set; }
         public User() { }
 
         public User(string lastName, string firstName, string middleName, string email, string password,
-                    string role, List<PreferenceTag>? preferences, string profession,
-                    int? roundtableId, List<BaseEvent>? events)
+                    string role, List<string>? preferences, string profession,
+                    int? roundtableId, List<ObjectId>? events)
         {
             LastName = lastName;
             FirstName = firstName;
